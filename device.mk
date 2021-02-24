9d97e003ef6d8983ae08c328b531eaded249f8be
micky387 <mickaelsaibi@free.fr>
2021-02-24 20:49:54 +0100

    rog2: Update proprietary files for A11
    Change-Id: I326c70bc5c02254af2ed22a8b635780f326c7329

--- device.mk
+++ device.mk
@@ -142,6 +142,8 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder
 
+PRODUCT_BOOT_JARS += android.hidl.manager-V1.0-java
+
 # Live Wallpapers
 PRODUCT_PACKAGES += \
     LiveWallpapers \
@@ -219,6 +221,10 @@ PRODUCT_PACKAGES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libnl
+    libnl \
+    libwfds
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
 
 include vendor/qcom/opensource/display-commonsys-intf/config/display-product-system.mk
