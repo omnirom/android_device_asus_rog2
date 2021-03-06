437a2b4166cb255a542f684c9c88861d5ffffcfd
micky387 <mickaelsaibi@free.fr>
2021-03-06 02:34:25 +0100

    rog2: DeviceParts: rewrite fps
    Change-Id: Ic9595f273026a24a8d55475d60a8186d1faf65c1

--- DeviceParts/src/org/omnirom/device/DeviceSettings.java
+++ DeviceParts/src/org/omnirom/device/DeviceSettings.java
@@ -19,11 +19,16 @@ package org.omnirom.device;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.os.ServiceManager;
 import android.os.SystemProperties;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.ListPreference;
@@ -39,6 +44,7 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.util.Log;
+import java.util.Map;
 
 public class DeviceSettings extends PreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -49,16 +55,19 @@ public class DeviceSettings extends PreferenceFragment implements
     private static final String KEY_CATEGORY_SCREEN = "screen";
     private static final String KEY_FRAME_MODE = "frame_mode_key";
     public static final String KEY_GAME_GENIE = "game_toolbar_app";
-    public static final String VENDOR_FPS = "vendor.asus.dfps";
-    public static final String TEMP_FPS = "temp_fps";
+    public static final String FPS = "fps";
 
-    public static final String DEFAULT_FPS_VALUE = "60";
-    public static final String FPS_VALUE_90 = "90";
-    public static final String FPS_VALUE_120 = "120";
+    protected static final String DEFAULT_FPS_VALUE = "60";
 
     private static ListPreference mFrameModeRate;
     private static TwoStatePreference mGloveModeSwitch;
     private static Preference mGameGenie;
+
+    private static final String SURFACE_FLINGER_SERVICE_KEY = "SurfaceFlinger";
+    private static final String SURFACE_COMPOSER_INTERFACE_KEY = "android.ui.ISurfaceComposer";
+    private static final int SURFACE_FLINGER_CODE = 1035;
+    private static Map<Integer, Integer> fpsMap = Map.of(120, 0, 90, 1, 60, 2);
+    private static IBinder mSurfaceFlinger;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -70,11 +79,11 @@ public class DeviceSettings extends PreferenceFragment implements
         mGloveModeSwitch.setOnPreferenceChangeListener(new GloveModeSwitch(getContext()));
 
         mFrameModeRate = (ListPreference) findPreference(KEY_FRAME_MODE);
+        int framevalue = Settings.System.getInt(getContext().getContentResolver(),
+                            FPS, 60);
+        mFrameModeRate.setValue(Integer.toString(framevalue));
+        mFrameModeRate.setSummary(mFrameModeRate.getEntry());
         mFrameModeRate.setOnPreferenceChangeListener(this);
-        int frameMode = getFrameMode(0);
-        int valueIndex = mFrameModeRate.findIndexOfValue(String.valueOf(frameMode));
-        mFrameModeRate.setValueIndex(valueIndex);
-        mFrameModeRate.setSummary(mFrameModeRate.getEntries()[valueIndex]);
 
         mGameGenie = findPreference(KEY_GAME_GENIE);
         mGameGenie.setEnabled(GameGenie.isGameGenieExist(this.getContext()));
@@ -89,46 +98,29 @@ public class DeviceSettings extends PreferenceFragment implements
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == mFrameModeRate) {
-            String value = (String) newValue;
-            int frameMode = Integer.valueOf(value);
-            setFrameMode(0, frameMode);
-            int valueIndex = mFrameModeRate.findIndexOfValue(value);
-            mFrameModeRate.setSummary(mFrameModeRate.getEntries()[valueIndex]);
+            int value = Integer.valueOf((String) newValue);
+            int index = mFrameModeRate.findIndexOfValue((String) newValue);
+            mFrameModeRate.setSummary(mFrameModeRate.getEntries()[index]);
+            changeFps(getContext(), value);
+            Settings.System.putInt(getContext().getContentResolver(), FPS, value);
         }
         return true;
     }
 
-    private int getFrameMode(int position) {
-
-        String value = Settings.System.getString(getContext().getContentResolver(), TEMP_FPS);
-        final String defaultValue = DEFAULT_FPS_VALUE;
-
-        if (value == null) {
-            value = defaultValue;
-        }
+    protected static void changeFps(Context context, int fps) {
+        mSurfaceFlinger = ServiceManager.getService(SURFACE_FLINGER_SERVICE_KEY);
         try {
-            String[] parts = value.split(",");
-            return Integer.valueOf(parts[position]);
-        } catch (Exception e) {
+            if (mSurfaceFlinger != null) {
+                mSurfaceFlinger = ServiceManager.getService(SURFACE_FLINGER_SERVICE_KEY);
+                Parcel data = Parcel.obtain();
+                data.writeInterfaceToken(SURFACE_COMPOSER_INTERFACE_KEY);
+                data.writeInt(fpsMap.getOrDefault(fps, -1));
+                mSurfaceFlinger.transact(SURFACE_FLINGER_CODE, data, null, 0);
+                data.recycle();
+            }
+        } catch (RemoteException ex) {
+               // intentional no-op
         }
-        return 0;
-    }
-
-    private void setFrameMode(int position, int fps) {
-
-        String value = Settings.System.getString(getContext().getContentResolver(), TEMP_FPS);
-        final String defaultValue = DEFAULT_FPS_VALUE;
-
-        if (value == null) {
-            value = defaultValue;
-        }
-        try {
-            String[] parts = value.split(",");
-            parts[position] = String.valueOf(fps);
-            String newValue = TextUtils.join(",", parts);
-            Settings.System.putString(getContext().getContentResolver(), TEMP_FPS, newValue);
-            SystemProperties.set(VENDOR_FPS, newValue);
-        } catch (Exception e) {
-        }
+            Settings.System.putInt(context.getContentResolver(), DeviceSettings.FPS, fps);
     }
 }
--- DeviceParts/src/org/omnirom/device/FrameRateTileService.java
+++ DeviceParts/src/org/omnirom/device/FrameRateTileService.java
@@ -16,13 +16,11 @@
 package org.omnirom.device;
 
 import android.graphics.drawable.Icon;
-import android.os.SystemProperties;
 import android.provider.Settings;
 import android.service.quicksettings.Tile;
 import android.service.quicksettings.TileService;
 
 public class FrameRateTileService extends TileService {
-    private static String VENDOR_FPS = "vendor.asus.dfps";
 
     private static final String DEFAULT_FPS_VALUE = "60";
     private static final String FPS_VALUE_90 = "90";
@@ -47,9 +45,11 @@ public class FrameRateTileService extends TileService {
     public void onStartListening() {
         super.onStartListening();
 
-        String value = Settings.System.getString(this.getContentResolver(), DeviceSettings.TEMP_FPS);
+        String value = Settings.System.getString(this.getContentResolver(), DeviceSettings.FPS);
+
+        getQsTile().setState(Tile.STATE_ACTIVE);
         switch (value) {
-			case DEFAULT_FPS_VALUE:
+            case DEFAULT_FPS_VALUE:
                 getQsTile().setIcon(Icon.createWithResource(this, R.drawable.ic_refresh_rate));
                 break;
             case FPS_VALUE_90:
@@ -59,7 +59,6 @@ public class FrameRateTileService extends TileService {
                 getQsTile().setIcon(Icon.createWithResource(this, R.drawable.ic_refresh_rate_120));
                 break;
         }
-        getQsTile().setState(Tile.STATE_ACTIVE);
         getQsTile().updateTile();
     }
 
@@ -72,22 +71,19 @@ public class FrameRateTileService extends TileService {
     public void onClick() {
         super.onClick();
 
-        String value = Settings.System.getString(this.getContentResolver(), DeviceSettings.TEMP_FPS);
+        String value = Settings.System.getString(this.getContentResolver(), DeviceSettings.FPS);
 
         switch (value) {
-			case DEFAULT_FPS_VALUE:
-                Settings.System.putString(this.getContentResolver(), DeviceSettings.TEMP_FPS, FPS_VALUE_90);
-                SystemProperties.set(VENDOR_FPS, FPS_VALUE_90);
+            case DEFAULT_FPS_VALUE:
+                DeviceSettings.changeFps(this, Integer.valueOf(FPS_VALUE_90));
                 getQsTile().setIcon(Icon.createWithResource(this, R.drawable.ic_refresh_rate_90));
                 break;
             case FPS_VALUE_90:
-                Settings.System.putString(this.getContentResolver(), DeviceSettings.TEMP_FPS, FPS_VALUE_120);
-                SystemProperties.set(VENDOR_FPS, FPS_VALUE_120);
+                DeviceSettings.changeFps(this, Integer.valueOf(FPS_VALUE_120));
                 getQsTile().setIcon(Icon.createWithResource(this, R.drawable.ic_refresh_rate_120));
                 break;
             case FPS_VALUE_120:
-                Settings.System.putString(this.getContentResolver(), DeviceSettings.TEMP_FPS, DEFAULT_FPS_VALUE);
-                SystemProperties.set(VENDOR_FPS, DEFAULT_FPS_VALUE);
+                DeviceSettings.changeFps(this, Integer.valueOf(DEFAULT_FPS_VALUE));
                 getQsTile().setIcon(Icon.createWithResource(this, R.drawable.ic_refresh_rate));
                 break;
         }
