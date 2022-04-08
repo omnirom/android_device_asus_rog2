f81c0f18c70faa9fb93a1cf0a41dc184df329415
micky387 <mickaelsaibi@free.fr>
2022-04-08 11:37:47 +0200

    rog2: Drop the old FOD stuff
    Change-Id: I8202a71e2a7c93eef80eeb3ea29f4affa45eb4b1

--- device.mk
+++ device.mk
@@ -124,10 +124,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += qcom.fmradio
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    omni.biometrics.fingerprint.inscreen@1.0-service.asus_msmnile
-
 # Frameworks
 PRODUCT_PACKAGES += \
     FrameworksResOverlay
