b37b36e5e701f59d83586e492bfa1900ed240fac
micky387 <mickaelsaibi@free.fr>
2022-05-04 19:12:47 +0200

    rog2: Switch to custom sepolicy path
    Change-Id: I45a4be4c62dfdcba6c9eec11021d11675da06e35

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -165,7 +165,7 @@ TARGET_USES_MKE2FS := true
 
 # Sepolicy
 include vendor/omni/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/SEPolicy.mk
+include device/qcom/sepolicy_custom/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/product/private
 
