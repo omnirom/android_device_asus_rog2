a730a04252fda478b8fae0141067303e08b89c2d
micky387 <mickaelsaibi@free.fr>
2022-07-21 15:45:34 +0200

    rog2: Commonize the Asus's msmnile stuff
    Change-Id: I1540990ae765be4c894e1cea28e8d40680c0ae01

--- device.mk
+++ device.mk
@@ -21,43 +21,16 @@
 #
 $(call inherit-product, vendor/asus/rog2/rog2-vendor.mk)
 
+# Inherit from asus sm8150-common
+$(call inherit-product, device/asus/sm8150-common/common.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    vendor/omni/overlay/CarrierConfig
-
-# A/B
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    system \
-    vbmeta
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/omnipreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    omnipreopt_script
-
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService
-
-# Api
-PRODUCT_SHIPPING_API_LEVEL := 28
+    $(LOCAL_PATH)/overlay
 
 # Asus Services
 PRODUCT_PACKAGES += asus-services
 PRODUCT_BOOT_JARS += asus-services
-
-# VNDK
-PRODUCT_EXTRA_VNDK_VERSIONS := 30
-PRODUCT_TARGET_VNDK_VERSION := 30
 
 # audio
 PRODUCT_COPY_FILES += \
@@ -77,53 +50,10 @@ PRODUCT_PACKAGES += BluetoothExt
 PRODUCT_PACKAGES += libbluetooth_qti
 PRODUCT_PACKAGES += vendor.qti.hardware.bluetooth_dun-V1.0-java
 
-# Boot control
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl.recovery \
-    bootctrl.msmnile.recovery
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
-
-# Camera
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4 \
-    android.hardware.camera.device@3.4
-
-# Charger images
-PRODUCT_PACKAGES += \
-    omni_charger_res_images \
-    animation.txt \
-    font_charger.png
-
 # DeviceParts
 PRODUCT_PACKAGES += \
     DeviceParts \
     OmniDisplayManager
-
-# Display
-PRODUCT_PACKAGES += \
-    libdisplayconfig.qti \
-    libgralloc.qti \
-    libion \
-    libqdMetaData \
-    libtinyalsa \
-    libtinyxml2
-
-$(call inherit-product, vendor/qcom/opensource/commonsys-intf/display/config/display-product-system.mk)
-$(call inherit-product, vendor/qcom/opensource/commonsys/display/config/display-product-commonsys.mk)
-
-# Exclude vibrator from InputManager
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:system/etc/excluded-input-devices.xml
-
-# FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    libqcomfm_jni \
-    qcom.fmradio
-
-PRODUCT_BOOT_JARS += qcom.fmradio
 
 # Frameworks
 PRODUCT_PACKAGES += \
@@ -148,27 +78,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keychars/goodix_ts.kcm:system/usr/keychars/goodix_ts.kcm \
     $(LOCAL_PATH)/keylayout/goodix_ts.kl:system/usr/keylayout/goodix_ts.kl
 
-# HIDL
-PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.manager@1.0 \
-    libhidltransport \
-    libhwbinder
-
-# Live Wallpapers
-PRODUCT_PACKAGES += \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    VisualizationWallpapers \
-    librs_jni
-
-# NFC
-PRODUCT_PACKAGES += \
-    NfcNci \
-    Tag \
-    SecureElement \
-    com.android.nfc_extras
-
 # Power Feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.controls.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.controls.xml
@@ -178,65 +87,9 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,device/asus/rog2/prebuilt/system,system) \
     $(call find-copy-subdir-files,*,device/asus/rog2/prebuilt/recovery,recovery/root)
 
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
-# Properties
-BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
-
-# Netutils
-PRODUCT_PACKAGES += \
-    netutils-wrapper-1.0 \
-    libandroid_net
-
-PRODUCT_PACKAGES += \
-    vndk_package
-
-# Telephony extension
-PRODUCT_PACKAGES += telephony-ext
-PRODUCT_BOOT_JARS += telephony-ext
-
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    RemovePackages
-
-# Shims
-PRODUCT_PACKAGES += \
-    lib-imsvt
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/libshim/lib-imsvideocodec.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/lib-imsvideocodec.so
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
-
-# Systemhelper
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.systemhelper@1.0
-
-# Telephony
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    tcmiface
-
-# Update engine
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
-PRODUCT_HOST_PACKAGES += \
-    brillo_update_payload
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
 
 # Vibrator
 PRODUCT_PACKAGES += \
@@ -245,16 +98,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vintf/manifest.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/vintf/manifest.xml \
     $(LOCAL_PATH)/vintf/vendor.qti.hardware.vibrator.service.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/vintf/manifest/vendor.qti.hardware.vibrator.service.xml
-
-# WiFi
-PRODUCT_PACKAGES += \
-    TetheringOverlay \
-    WifiOverlay
-
-# Wifi Display
-PRODUCT_PACKAGES += \
-    libavservices_minijail \
-    libnl \
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
